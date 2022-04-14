3ef123dd059735da0b6e0d683fb8590947e90061
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-15 00:54:30 +0600

    Header Responsive

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../images/logo2.png";
-import { BsCart2 } from "react-icons/bs";
+import { BiMenuAltRight, BiCart } from "react-icons/bi";
 
 const Header = () => {
+  const toggleMenu = () => {
+    const menu = document.querySelector("#menu");
+    menu.classList.toggle("-translate-x-96");
+  };
   return (
-    <div className="container mx-auto flex justify-between items-center p-4">
-      <div className="w-36">
-        <Link to="/">
-          <img src={logo} alt="Logo" />
-        </Link>
+    <div className="container mx-auto flex flex-col md:flex-row md:justify-between md:items-center p-4">
+      <div className="flex items-center justify-between">
+        <div className="w-36">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+        <div className="flex items-center gap-2 text-3xl md:hidden block">
+          <Link to="/cart" className="cursor-pointer">
+            <BiCart />
+          </Link>
+          <BiMenuAltRight onClick={toggleMenu} className="cursor-pointer" />
+        </div>
       </div>
-      <menu className="flex items-center gap-8">
+      <menu
+        id="menu"
+        className="-translate-x-96 md:transform-none h-full w-1/2 md:w-auto md:h-auto bg-white drop-shadow-lg md:drop-shadow-none md:bg-transparent absolute md:static transition-all translate-10 flex flex-col md:flex-row items-center gap-8"
+      >
         <li>
-          <Link to="/cart">
-            <BsCart2 />
+          <Link to="/cart" className="cursor-pointer text-3xl hidden md:block">
+            <BiCart />
           </Link>
         </li>
         <li>
