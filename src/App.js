a5a69adbaba4ed5c5ef811a7baeeb337d5ac29f8
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-04-15 00:06:37 +0600

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/common/Footer/Footer";
+import Header from "./components/common/Header/Header";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
